79a703ed70cd7cfb8e5ed0c0ad195b78a41c7bd8
AntonShih <anton.shih7@gmail.com>
2025-04-07 14:38:05 +0800

    最初LOOP檔

---
+++ Cumulative_loop.py
@@ -0,0 +1,8 @@
+total = 0
+i = 0
+while i < 10 : #while 是 迴圈 的意思，也就是說「當某個條件成立，就重複執行裡面的程式碼」。
+	total = total + (i+1) #兩行縮排表示它們是「while 迴圈裡要執行的程式」
+	print(total) #在迴圈裡面會印10次
+	i = i+1
+	
+print("!!!",total) #在外面只印一次
