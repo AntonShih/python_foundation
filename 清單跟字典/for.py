6317fce1578912086b1c1cc14ccb41585ca19c55
AntonShih <anton.shih7@gmail.com>
2025-04-08 14:55:59 +0800

    for、range

---
+++ 清單跟字典/for.py
@@ -0,0 +1,37 @@
+# scores = [30,50,80,60,90]
+# i=0
+
+# for score in scores:
+#     if score >= 60:
+#         i = i+1
+# print("及格人數:",i)
+# #--------------------------------------------------------
+
+# scores = [30,50,80,60,90]
+# i=0
+# count = 0
+
+# while i < len(scores): #i<5 ->0.1.2.3.4
+#     print (scores[i])
+#     if scores[i] >= 60:
+#         count += 1
+#     i = i+1
+# print("及格人數:",count)
+#----------------------------------------------------------
+
+# 1加到10
+total = 0
+
+for a in range(10):
+    total = total + (a+1)
+    print(a+1)
+print(total)
+
+#--------------------------------------------------------
+# total = 0
+# i = 0
+# while i < 10  :
+#     total = total + (i+1)
+#     print(total)
+#     i = i + 1
+# print(total)
